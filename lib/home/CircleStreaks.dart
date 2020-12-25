843f4ed86b76faa6b1c540ed342a409c5c654e3d
yurylink <yurylink@hotmail.com>
2020-12-25 23:09:07 +0000

    Modifying the Main circle on the Home page

--- lib/home/CircleStreaks.dart
+++ lib/home/CircleStreaks.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:iquit/model/StreaksDurationModel.dart';
 
 // class CircleStreaks extends StatefulWidget {
@@ -36,7 +37,7 @@ class CircleStreaks extends StatelessWidget {
   String _displayText;
 
   CircleStreaks(String textSent) {
-    this._displayText = textSent??"No Streak Initialized inner";
+    this._displayText = textSent ?? "No Streak Initialized inner";
   }
 
   @override
@@ -46,8 +47,15 @@ class CircleStreaks extends StatelessWidget {
         height: 230,
         decoration:
             new BoxDecoration(color: Colors.grey, shape: BoxShape.circle),
-        child: Center(
-          child: Text(_displayText),
+        padding: EdgeInsets.all(20),
+        child: Container(
+          width: 50,
+          height: 50,
+          decoration: new BoxDecoration(
+              color: Color.fromRGBO(46, 46, 46, 1), shape: BoxShape.circle),
+          child: Center(
+            child: Text(_displayText),
+          ),
         ));
   }
 }
